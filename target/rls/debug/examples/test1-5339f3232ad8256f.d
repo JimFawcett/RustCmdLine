181dd15beeb11547ec4ae1fc0123bf18f9646a94
James W. Fawcett <jfawcett@twcny.rr.com>
2020-05-09 13:18:50 -0400

    updated version number

--- target/rls/debug/examples/test1-5339f3232ad8256f.d
+++
@@ -1,5 +0,0 @@
-c:\github\JimFawcett\RustCmdLine\target\rls\debug\examples\test1-5339f3232ad8256f.rmeta: examples\test1.rs
-
-c:\github\JimFawcett\RustCmdLine\target\rls\debug\examples\test1-5339f3232ad8256f.d: examples\test1.rs
-
-examples\test1.rs:
