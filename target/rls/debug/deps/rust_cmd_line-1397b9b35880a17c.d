ce0e4a3c6443e7e612b420f3bfb9676996ec7784
James W. Fawcett <jfawcett@twcny.rr.com>
2020-05-31 20:59:39 -0400

    cleaned

--- target/rls/debug/deps/rust_cmd_line-1397b9b35880a17c.d
+++
@@ -1,5 +0,0 @@
-c:\github\JimFawcett\RustCmdLine\target\rls\debug\deps\rust_cmd_line-1397b9b35880a17c.rmeta: src\lib.rs
-
-c:\github\JimFawcett\RustCmdLine\target\rls\debug\deps\rust_cmd_line-1397b9b35880a17c.d: src\lib.rs
-
-src\lib.rs:
